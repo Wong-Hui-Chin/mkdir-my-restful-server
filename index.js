f20c0d571cd130326c462d7990ceb0e1f73fc570
Wong-Hui-Chin <b022210255@student.utem.edu.my>
2024-04-17 09:29:44 +0800

    commit

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
