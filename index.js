aad794250429f5fcf3c63a3cc2e7afe4f1fba018
Wong-Hui-Chin <b022210255@student.utem.edu.my>
2024-04-17 22:02:48 +0800

    commit

--- index.js
+++ index.js
@@ -59,13 +59,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // Send a ping to confirm a successful connection
     await client.connect();
-    //let result = await client.db('BERRDB').collection('students').deleteOne(
-    //  { _id: new ObjectId('660517558d711e579b1621ff')},
-    //)
-    //console.log('Connected successfully to MongoDB')
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
